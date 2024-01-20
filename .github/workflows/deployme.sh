432054f7b06221437e6d27bf4d10146b7998f84d
tanishqsoni0309 <tanishqsoni0309@gmail.com>
2024-01-20 23:24:23 +0530

    adding CI/CD

---
+++ .github/workflows/deployme.sh
@@ -0,0 +1,13 @@
+server_address="tanishq@64.227.160.102"
+password="MyPass@12345"
+
+sshpass -p "$password" ssh "$server_address" <<EOF 
+    cd ./restrauntify 
+    git pull origin main 
+    cd ./server_main/ 
+    source ./env/bin/activate 
+    pip3 install -r requirements.txt
+    python3 manage.py makemigrations
+    python3 manage.py migrate
+    echo "$password" | sudo -S systemctl restart gunicorn
+EOF

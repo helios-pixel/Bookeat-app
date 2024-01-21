1898340daa3902acc82d4e8b8f4f5842a8fa9152
tanishqsoni0309 <tanishqsoni0309@gmail.com>
2024-01-21 17:53:32 +0530

    adding cors headers

--- server_main/server_mains/settings.py
+++ server_main/server_mains/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'authApp',
     'restaurant',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'server_mains.urls'
 
